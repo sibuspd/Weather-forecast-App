679928daa1f3678e14d967d0b0128d1dbbcf68c5
sibuspd <sibuspd@gmail.com>
2024-09-24 16:47:52 +0530

    AirQuality Index module completed

--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ const error404 = document.querySelector(".not-found");
 const getLocation = document.getElementById("location-button");
 const topContainer = document.getElementById('t-container');
 const bottomContainer = document.getElementById('b-container');
-const forecastButton = document.getElementById('forecast-button');
+const cityButton = document.getElementById('cities-button');
 
-let lon,lat,pressure,visibility,sunrise,sunset,City;
+const APIKey ='a51436fe16293393a10f617c7eaf0bca';
+
+let lon, lat, pressure, visibility, sunrise, sunset, timezone, City;
 
 let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
 currentWeatherCard = document.querySelectorAll('.weather-left .card')[0];
 const forecastCard = document.querySelector('.day-forecast');
+let aqiCard = document.querySelectorAll('.highlight .card')[0];
+// console.log(aqiCard);
+let dayCard = document.querySelectorAll('.highlight .card')[1];
+    console.log(dayCard);
 
 search.addEventListener('click',()=>{
 
-    const APIKey ='a51436fe16293393a10f617c7eaf0bca';
     const city = document.querySelector('.search-box input').value;
     City = city;
 
@@ -55,10 +60,6 @@ search.addEventListener('click',()=>{
 
             lon = json.coord.lon;
             lat = json.coord.lat;
-            pressure = json.main.pressure;
-            visibility = json.visibility;
-            sunrise = json.sys.sunrise;
-            sunset = json.sys.sunset; 
             
             switch(json.weather[0].main){ // Scans the JSON data of a particular City
                 case 'Clear':
@@ -101,8 +102,11 @@ search.addEventListener('click',()=>{
             container.style.height = '590px'; // Readjusting container's height
 
             // Forecast in Left Section
-
-            displayForecast(json, lat, lon);
+            let temp = json.main.temp;
+            displayForecast(json, lat, lon,temp);
+            displayAQ(lat, lon);            
+            getAmbienceData(city);
+    
     })
 });
 
@@ -112,7 +116,7 @@ getLocation.addEventListener('click', async () => {
 
 function gotLocation(position){
     console.log("Location tracked");
-
+    // console.log (position);
     const APIKey = `a51436fe16293393a10f617c7eaf0bca`;
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
@@ -122,7 +126,7 @@ function gotLocation(position){
         if(json.cod === '404') // HTTP Status code 404 - Resource not found
             console.log('Failed to track location');   
             
-            // console.log(json);
+            // console.log(json); // json here is lat and lon based data
             
             topContainer.style.height = '550px';
             bottomContainer.style.display = 'none';
@@ -162,7 +166,10 @@ function gotLocation(position){
                     localImage.src = '';
             }
 
-        displayForecast(json, lat, lon);
+        const temp = json.main.temp-273.15;    
+        displayForecast(json, lat, lon,temp);
+        displayAQ(lat, lon);
+        getAmbienceData(json.name);
     });    
 }
 
@@ -170,18 +177,18 @@ function failedLocation(){
     alert("Access denied. Failed to trace location");
 }
 
-function displayForecast(data, lat, lon){
+function displayForecast(data, lat, lon,temp){
     const APIKey ='a51436fe16293393a10f617c7eaf0bca';
-    console.log(lat, lon);
+    // console.log(lat, lon);
     let date = new Date();
 
     let forecast_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 
     currentWeatherCard.innerHTML = `<div class="current-weather flex justify-between items-center">
                         <div class="details">
-                            <p class=" text-white text-sm">Now</p>
-                            <h2 class="text-white text-3xl mx-2">${data.main.temp.toFixed(2)}&deg;C</h2>
-                            <p class="text-gray-400 text-sm mx-2">${data.weather[0].description}</p>
+                            <p class=" text-orange-300 text-sm">Now</p>
+                            <h2 class="text-white text-3xl mx-2"> ${temp.toFixed(2)}&deg;C</h2>
+                            <p class="text-gray-400 text-sm mx-2"> ${data.weather[0].description}</p>
                         </div>
                         <div class="weather-icon">
                             <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}" alt="">
@@ -189,12 +196,12 @@ function displayForecast(data, lat, lon){
                     </div>
                     <hr class="my-1 border-t-2 ">
                     <div class="card-footer text-sm">
-                        <p class="text-gray-100 mb-3"><i class="fa-regular fa-calendar text-white text-lg"></i>${days[date.getDay()]}, ${date.getDate()}, ${months[date.getMonth()]} </p>
-                        <p class="text-gray-100 mb-3"><i class="fa-solid fa-location-dot text-white text-lg"></i>${data.name}, ${data.sys.country}</p>
+                        <p class="text-gray-100 mb-3"><i class="fa-regular fa-calendar text-sky-200 text-lg"></i>  ${days[date.getDay()]}, ${date.getDate()}, ${months[date.getMonth()]} </p>
+                        <p class="text-gray-100 mb-3"><i class="fa-solid fa-location-dot text-green-300 text-lg"></i>  ${data.name}, ${data.sys.country}</p>
                     </div>`;
 
     fetch(forecast_URL).then(res => res.json()).then(dataII => {
-        // console.log(dataII);
+        // console.log(dataII); // dataII is forecast related data 
         let uniqueDays = [];
         let fiveDays = dataII.list.filter(forecast => {
             let forecastDate = new Date(forecast.dt_txt).getDate();
@@ -215,7 +222,108 @@ function displayForecast(data, lat, lon){
                             <p class="text-gray-200">${days[date.getDay()]}</p>
         </div>`;
         }
+
+        console.log("Display Forecast CB called");
+                    
     }).catch(()=>{
         alert("Failed to fetch forecast data.");
     });
+}
+
+function displayAQ(lat, lon){
+
+    let APIKey = 'a51436fe16293393a10f617c7eaf0bca';
+    let aqi_URL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+
+    fetch(aqi_URL).then(res => res.json()).then(dataIII => {
+        // console.log(dataIII); // dataIII is Air Quality related data
+        let {co, no, no2, o3, so2, pm2_5, pm10, nh3} = dataIII.list[0].components; // Object destructuring
+        // console.log(co,no,no2, o3, so2, pm2_5, pm10, nh3);
+        let aqiScore = ['Good','Fair','Moderate','Poor','Very Poor'];
+
+        aqiCard.innerHTML = `
+        <div class="card-head flex gap-28 justify-between mb-3 p-3 rounded-xl bg-gray-800 bg-opacity-20">
+                    <p class="text-white">Score</p>
+                    <p class="air-index aqi-${dataIII.list[0].main.aqi} bg-yellow-300 text-black text-sm font-semibold rounded-2xl px-1 py-1">${aqiScore[dataIII.list[0].main.aqi - 1]}</p> 
+                </div> 
+                <div class="stats grid grid-cols-2 place-items-center">                    
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center ">PM2.5</p>
+                        <h2 class="text-white text-2xl">${pm2_5}</h2>
+                    </div>
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center">PM10</p>
+                        <h2 class="text-white text-2xl">${pm10}</h2>
+                    </div>
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center">S02</p>
+                        <h2 class="text-white text-2xl">${so2}</h2>
+                    </div>
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center">CO</p>
+                        <h2 class="text-white text-2xl">${co}</h2>
+                    </div>
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center">NO</p>
+                        <h2 class="text-white text-2xl">${no}</h2>
+                    </div>
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center">NO2</p>
+                        <h2 class="text-white text-2xl">${no2}</h2>
+                    </div>
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center">NH3</p>
+                        <h2 class="text-white text-2xl">${nh3}</h2>
+                    </div>
+                    <div class="item text-center">
+                        <p class="text-gray-400 text-sm font-semibold text-center">O3</p>
+                        <h2 class="text-white text-2xl">${o3}</h2>
+                    </div>    
+                </div>`;
+        console.log("display AQI CB called");
+
+    }).catch(() => {alert("Failed to fetch Air Quality data for the location.")});
+}
+
+function getAmbienceData(city){
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(dataIV => {
+        pressure = dataIV.main.pressure;
+        visibility = dataIV.visibility;
+        sunrise = dataIV.sys.sunrise;
+        sunset = dataIV.sys.sunset;
+        timezone = dataIV.timezone;
+        // console.log(pressure,visibility,sunrise,sunset,timezone);
+        let sRiseTime = moment.utc(sunrise, 'X').add(timezone, 'seconds').format('hh: mm A'),
+        sSetTime = moment.utc(sunset, 'X').add(timezone, 'seconds').format('hh: mm A');
+
+        console.log("Sunrise Sunset CB called");
+
+        dayCard.innerHTML = `
+        <div class="card-head">
+            <p class="text-white text-xl mb-3 py-2 pt-5 text-center font-medium">Sunrise & Sunset</p>
+        </div>
+        <div class="sunrise-sunset grid grid-cols-2 place-items-center">
+            <div class="item flex items-center gap-3">
+            <div class="icon">
+                <i class="fa-solid fa-sun fa-2x text-yellow-300"></i>
+            </div>
+            <div>
+                <p class="text-gray-200">Sunrise</p>
+                <h2 class="mt-4 text-white">${sRiseTime}</h2>
+            </div>    
+        </div>
+        <div class="item flex items-center gap-3">
+            <div class="icon">
+                <i class="fa-solid fa-star fa-2x text-violet-300"></i>
+            </div>
+            <div>
+                <p class="text-gray-200">Sunset</p>
+                <h2 class="mt-4 text-white">${sSetTime}</h2>
+            </div>    
+        </div>
+        </div>`;
+    })
 }
